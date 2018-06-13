be9f0fabe18fbf22e3d3e54c5ef639cb8feedbe8
Akira Moroo <retrage01@gmail.com>
2018-06-13 15:10:05 +0900

    Fix to create cargo project

--- tools/runrs.sh
+++ tools/runrs.sh
@@ -2,8 +2,14 @@
 
 set -e
 
-tmp=`echo $1 | sed 's/\./_/g'`
-cp $1 $tmp
-rustc $tmp -o $1.exe
-rm $tmp
-./$1.exe
+$name=`basename -s .rs $1 | xargs basename -s .eir`
+mkdir -p "/tmp/$name/src"
+cp $1 "/tmp/$name/src/main.rs"
+echo "[package]\n" >> "/tmp/$name/Cargo.toml"
+echo "name = \"$name\"\n" >> "/tmp/$name/Cargo.toml"
+echo "version = \"0.1.0\"\n" >> "/tmp/$name/Cargo.toml"
+echo "\n" >> "/tmp/$name/Cargo.toml"
+echo "[dependencies]\n" >> "/tmp/$name/Cargo.toml"
+echo "lib = \"0.2\"" >> "/tmp/$name/Cargo.toml"
+cd "/tmp/$name"
+cargo run --release --quiet
